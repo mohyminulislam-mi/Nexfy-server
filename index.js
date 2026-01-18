842ee4438429d583d8e6f46147666966a243e7e7
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-01-18 10:12:46 +0600

    make products get apis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import dependencies
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 // Create app
 const app = express();
@@ -10,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@mohyminulislam.uwhwdlk.mongodb.net/?appName=Mohyminulislam`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,22 +27,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("Nexfy");
+    const productsCollection = database.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/products/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("✅ Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 // Default route
 app.get("/", (req, res) => {
-  res.send("Hello Admin! 🚀");
+  res.send("Hello Admin! ✅");
 });
 
 // Start server
 app.listen(port, () => {
-  console.log(`✅ Server running : ${port}`);
+  console.log(`🚀 Server running : ${port}`);
 });
